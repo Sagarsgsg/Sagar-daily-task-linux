fe990e0a6beda49b9a5fecf96d23e0288f6393ab
Sagar Devops <135566555+Sagarsgsg@users.noreply.github.com>
2023-11-26 20:21:56 +0530

    Create function.sh

---
+++ function.sh
@@ -0,0 +1,18 @@
+function welcome(){
+		ech0 "commands to welcome";
+	}
+	welcome #call the function#
+
+
+function welcome(){
+		ech0 "$1";
+	}
+	welcome linux
+	welcome devops
+	welcome AWS
+
+
+	O/P::-	
+	linux
+	devops
+	AWS
