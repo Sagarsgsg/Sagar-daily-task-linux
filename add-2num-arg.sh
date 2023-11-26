a6413ffec6fd8bcd75737236a59249628af07a58
Sagar Devops <135566555+Sagarsgsg@users.noreply.github.com>
2023-11-26 20:14:13 +0530

    Create add-2num-arg.sh

---
+++ add-2num-arg.sh
@@ -0,0 +1,12 @@
+  echo"enter a value: "
+	read a
+	echo"enter b value: "
+	read b
+	c=$(($a+$b))
+	
+	echo "sum of $a and $b is : $c"
+	# if u want to exit from the loop use END#
+	
+	O/P::-
+	
+	sum of 65 and 78 is : 143
