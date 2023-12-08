dad286bfb06ea1c7b126751806507b961f49e93e
Sagar Devops <135566555+Sagarsgsg@users.noreply.github.com>
2023-12-08 18:34:57 +0530

    Create Forloop-directory -check.sh

---
+++ Forloop-directory -check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for folder in $(find -type d);
+do
+	echo "the folder is $folder"
+	if[ -d test ];
+	then
+			echo"this folder is exist"
+			echo "removing the folder"
+			rm -rf test
+	else
+			echo"test folder does not exist"
+			
+	fi
